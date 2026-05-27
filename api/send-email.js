e3856c85174ce9ecf9dc623a5584e11bf52e5b11
arnseyy78-stack <arnseyy78@gmail.com>
2026-05-27 19:39:56 +0800

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -31,13 +31,12 @@ export default async function handler(req, res) {
 
           <p>Hi ${studentName || "Student"},</p>
 
-          <p>Your booking has been confirmed.</p>
+<p>Your purchase has been confirmed.</p>
 
-          <p><b>Package:</b> ${packageName}</p>
-          <p><b>Class:</b> ${className}</p>
-          <p><b>Amount:</b> ${amount}</p>
-          <p><b>Credits:</b> ${credits}</p>
-          <p><b>Expiry:</b> ${expiry}</p>
+<p><b>Package:</b> ${packageName}</p>
+<p><b>Amount:</b> ${amount}</p>
+<p><b>Credits:</b> ${credits}</p>
+<p><b>Expiry:</b> ${expiry}</p>
 
           <br/>
 
