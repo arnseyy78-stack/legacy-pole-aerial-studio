4f151a47a470618982c28c02a909397141e7bcc9
arnseyy78-stack <arnseyy78@gmail.com>
2026-05-30 01:07:42 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1416,7 +1416,7 @@ const goldSmallText = {
 };
 
 const goldLine = {
-  width: "220px",
+  width: "500px",
   height: "2px",
   background:
     "linear-gradient(90deg, rgba(200,169,107,1) 0%, rgba(200,169,107,0.4) 100%)",
