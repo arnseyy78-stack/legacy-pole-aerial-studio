cf71ebb320404922f13d08ad23a4999edd3755ca
arnseyy78-stack <arnseyy78@gmail.com>
2026-06-21 09:37:45 +0800

    Update send-verification-code.js

--- api/send-verification-code.js
+++ api/send-verification-code.js
@@ -13,12 +13,12 @@ export default async function handler(req, res) {
     }
 
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
+      host: smtp-relay.brevo.com,
+      port: 587,
       secure: false,
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
+        user: af6028001@smtp-brevo.com,
+        pass: M4GadASr6YqKNZcp
       }
     });
 
