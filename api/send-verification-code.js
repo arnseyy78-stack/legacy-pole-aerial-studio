1c12007b81774f065f6b1834544808df0cffd6d6
arnseyy78-stack <arnseyy78@gmail.com>
2026-06-21 09:15:05 +0800

    Update send-verification-code.js

--- api/send-verification-code.js
+++ api/send-verification-code.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
