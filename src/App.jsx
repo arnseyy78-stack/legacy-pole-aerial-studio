ea637d4f45a6a232bfe65f468745ecf3644983e5
arnseyy78-stack <arnseyy78@gmail.com>
2026-07-09 08:48:06 +0800

    Change amount for Single Pass from 87000 to 100

--- src/App.jsx
+++ src/App.jsx
@@ -2691,7 +2691,7 @@ setPage("bookingConfirmed");
             price: "₱870",
             description: "1 class credit",
             subText: "Pole or Aerial.",
-            amount: 87000,
+            amount: 100,
             credits: 1,
             expiry: "30 Days"
           },
