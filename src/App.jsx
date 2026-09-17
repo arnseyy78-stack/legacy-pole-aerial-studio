2d3970e0ea0506a542b8fcd89049c11025c78bda
arnseyy78-stack <arnseyy78@gmail.com>
2026-09-17 16:44:42 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2255,43 +2255,6 @@ setPage("bookingConfirmed");
     >
       Instructor: London
     </p>
-  </>
-)}
-                <p
-  style={{
-    color: "#c8a96b",
-    marginTop: "12px"
-  }}
->
-{(() => {
-  const bookedCount =
-    bookedSlots[
-      `${currentMonthName}-${selectedDate}-${item[1]}`
-    ] || 0;
-
-  const maxSlots = Number(item[3]) || 5;
-const spotsLeft = maxSlots - bookedCount;
-
-  return (
-    <>
-      <p style={{ color: "#c8a96b", marginTop: "12px" }}>
-        {bookedCount} / {maxSlots} booked
-      </p>
-
-      <p
-        style={{
-          color: spotsLeft <= 1 ? "#ff6b6b" : "#999",
-          fontWeight: spotsLeft <= 1 ? "bold" : "normal"
-        }}
-      >
-        {spotsLeft > 0
-          ? `${spotsLeft} spot${spotsLeft === 1 ? "" : "s"} left`
-          : "FULL"}
-      </p>
-    </>
-  );
-})()}
-</p>
               </button>
             ))}
           </div>
